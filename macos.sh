9bd760dcb9d5d9f654f5da28af3a47cc9c98c566
Leo Weigand <leo@weigand.io>
2022-10-09 22:13:17 +0200

    add macOS settings

---
+++ macos.sh
@@ -0,0 +1,32 @@
+osascript -e 'tell application "System Preferences" to quit'
+# sudo -v
+
+##############################
+# Trackpad and Keyboard      #
+##############################
+
+# Enable full keyboard access for all controls
+# (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Shorter delay for key repeat
+defaults write -g InitialKeyRepeat -int 10
+defaults write -g KeyRepeat -int 4
+
+# Faster trackpad speed
+defaults write -g com.apple.trackpad.scaling -float 7
+
+
+##############################
+# Dock                       #
+##############################
+
+# Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+
+# Minimize windows into their application’s icon
+defaults write com.apple.dock minimize-to-application -bool true
+
+
+
+killall Dock
