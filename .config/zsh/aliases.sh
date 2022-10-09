c89ba70a3f29af0d1d92b8505656371428a64395
Leo Weigand <leo@weigand.io>
2022-10-09 14:05:56 +0200

    remove unneeded aliases

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -1,10 +1,3 @@
 alias j="jump"
 alias vi="nvim"
 alias bm="bookmark"
-alias yadm="yadm --yadm-repo ~/git/dotfiles"
-
-# @todo not sure these should be here as they depend
-# on more specific tools being installed
-alias png="pngquant" # compress PNGs
-alias yt="youtube-dl --retries 3"
-alias ccat='pygmentize -O style=monokai -f console256 -g'
