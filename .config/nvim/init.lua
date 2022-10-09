a67a43d8d1ee24dd41d7fb96780a412d391d6c33
Leo Weigand <leo@weigand.io>
2022-10-09 15:12:32 +0200

    remove old nvim config

--- .config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
-require "plugins"
-require "settings"
-require "keymappings"
-require "statusline"
-require "lsp"
-
--- Set color cheme
-vim.g.sonokai_style = "andromeda"
-vim.cmd('colorscheme sonokai')
