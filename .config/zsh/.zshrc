fb83d344549d2ff075a8d1755262d007f6a979d5
Leo Weigand <leo@weigand.io>
2022-10-09 22:41:38 +0200

    fix kitty shell integration

--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -1,3 +1,9 @@
+# Set PATH, MANPATH, etc., for Homebrew.
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+XDG_CONFIG_HOME="$HOME/.config"
+
 # Main config file for ZSH
 
+. $ZDOTDIR/kitty.sh
 . $ZDOTDIR/aliases.sh
---
+++ .config/zsh/kitty.sh
@@ -0,0 +1,7 @@
+# Load Kitty shell integration
+if test -n "$KITTY_INSTALLATION_DIR"; then
+    export KITTY_SHELL_INTEGRATION="enabled"
+    autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
+    kitty-integration
+    unfunction kitty-integration
+fi
