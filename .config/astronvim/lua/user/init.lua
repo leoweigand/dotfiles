a1a85aa79d7bec981a3a2b16c7fb91ff55646ccf
Leo Weigand <leo@weigand.io>
2022-10-12 22:01:35 +0200

    add astronvim config

---
+++ .config/astronvim/lua/user/init.lua
@@ -0,0 +1,40 @@
+return {
+  -- Set colorscheme to use
+  colorscheme = "sonokai",
+
+  -- Set dashboard header
+  header = {
+    "██      ███████  ██████  ██    ██ ██ ███    ███",
+    "██      ██      ██    ██ ██    ██ ██ ████  ████",
+    "██      █████   ██    ██ ██    ██ ██ ██ ████ ██",
+    "██      ██      ██    ██  ██  ██  ██ ██  ██  ██",
+    "███████ ███████  ██████    ████   ██ ██      ██",
+  },
+
+  -- set vim options here (vim.<first_key>.<second_key> =  value)
+  options = {
+    g = {
+      sonokai_style = "shusia", -- select color scheme variant
+    },
+  },
+
+  -- Configure plugins
+  plugins = {
+    init = {
+      -- Color scheme
+      { "sainnhe/sonokai" },
+      { "RRethy/nvim-treesitter-textsubjects" },
+    },
+    bufferline = {
+      options = {
+        show_close_icon = false,
+        show_buffer_close_icons = false,
+      },
+    },
+    ["neo-tree"] = {
+      window = {
+        hide_dotfiles = false,
+      },
+    },
+  },
+}
