b16c5c29f131c5316de297646d1397eb577615ba
Leo <leo.weigand@n26.com>
2022-10-24 18:18:07 +0200

    add git_files Telescope mapping

--- .config/astronvim/lua/user/mappings.lua
+++ .config/astronvim/lua/user/mappings.lua
@@ -2,8 +2,11 @@ local hop = require "hop"
 
 return {
   n = {
-    ["S"] = {
+    S = {
       function() hop.hint_char2() end, desc = "Hop",
-    }
+    },
+    ["<leader>fg"] = {
+      function() require "telescope.builtin".git_files() end, desc = "Search git files",
+    },
   }
 }
