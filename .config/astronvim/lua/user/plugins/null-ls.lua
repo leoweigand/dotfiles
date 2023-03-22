f3a820a9b3850fda3af236a3fa7efaab8c1a92cd
Leo <leo.weigand@n26.com>
2023-03-22 15:31:08 +0100

    null-ls config

---
+++ .config/astronvim/lua/user/plugins/null-ls.lua
@@ -0,0 +1,20 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  opts = function(_, config)
+    local null_ls = require "null-ls"
+
+    -- supported formatters and linters:
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+    
+    config.sources = {
+      -- Set a formatter
+      null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.stylua,
+      -- Set a linter
+      -- null_ls.builtins.diagnostics.eslint,
+    }
+
+    return config 
+  end,
+}
