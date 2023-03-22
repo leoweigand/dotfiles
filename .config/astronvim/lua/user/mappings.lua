4b63911b2776ea107e01684e736f480fe109333b
Leo <leo.weigand@n26.com>
2023-03-22 15:29:53 +0100

    set up gitlinker

--- .config/astronvim/lua/user/mappings.lua
+++ .config/astronvim/lua/user/mappings.lua
@@ -1,12 +1,36 @@
-local hop = require "hop"
-
 return {
-  n = {
-    S = {
-      function() hop.hint_char2() end, desc = "Hop",
-    },
-    ["<leader>fg"] = {
-      function() require "telescope.builtin".git_files() end, desc = "Search git files",
-    },
-  }
+	n = {
+		["<leader>gy"] = {
+			function()
+				require("gitlinker").get_buf_range_url("n")
+			end,
+			desc = "Copy link to line on remote",
+		},
+		["<leader>go"] = {
+			function()
+				require("gitlinker").get_buf_range_url(
+					"n",
+					{ action_callback = require("gitlinker.actions").open_in_browser }
+				)
+			end,
+			desc = "Open line on remote",
+		},
+	},
+	v = {
+		["<leader>gy"] = {
+			function()
+				require("gitlinker").get_buf_range_url("v")
+			end,
+			desc = "Copy link to range on remote",
+		},
+		["<leader>go"] = {
+			function()
+				require("gitlinker").get_buf_range_url(
+					"v",
+					{ action_callback = require("gitlinker.actions").open_in_browser }
+				)
+			end,
+			desc = "Open range on remote",
+		},
+	},
 }
