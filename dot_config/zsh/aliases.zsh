c96437e176523cb11d8d946b40aaae2e2afb89d5
Leo Weigand <5489276+leoweigand@users.noreply.github.com>
2025-10-18 18:52:40 +0200

    feat: Add common shell and git aliases

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -4,11 +4,19 @@
 alias vim='nvim'
 alias vi='nvim'
 
+# Listing
+alias l='ls -l'
+alias la='ls -la'
+
 # Tools
 alias cat='bat'
+alias f='fzf'
+alias o='open .'
 
 # Git
 alias lg='lazygit'
+alias gs='git status'
+alias gp='git pull'
 
 # Claude Code
 alias lfg='claude --dangerously-skip-permissions'
